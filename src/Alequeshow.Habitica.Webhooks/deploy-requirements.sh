b2683a52d451184439ab7fcc02875ecf71f7bdc7
Alex Almeida <alex.pans@gmail.com>
2025-08-13 15:45:18 +0000

    Attempt to fix az login in deploy workflow
    Signed-off-by: Alex Almeida <alex.pans@gmail.com>

--- src/Alequeshow.Habitica.Webhooks/deploy-requirements.sh
+++ src/Alequeshow.Habitica.Webhooks/deploy-requirements.sh
@@ -4,7 +4,7 @@
 set -e
 
 # Install Azure CLI on Linux
-set -ecurl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 
 # Install Azure Functions Core Tools
 npm install -g azure-functions-core-tools@4 --unsafe-perm true
